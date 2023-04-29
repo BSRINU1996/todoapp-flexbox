81c6792b23a6250563a29765f2d9241cb308936c
MeSrinu <balisrinu1996@gmail.com>
2023-04-29 13:31:40 +0530

    developed an to-do-static-application using CSS Flexbox.

---
+++ index.js
@@ -0,0 +1,12 @@
+// let checkboxEle = document.createElement("input");
+// checkboxEle.type = "checkbox";
+// checkboxEle.id = "myCheckbox";
+// checkboxEle.checked = true;
+// let tasksContainer = document.getElementById("tasks-added");
+// tasksContainer.appendChild(checkboxEle);
+
+// let labelElement = document.createElement("label");
+// // labelElement.htmlFor = "myCheckbox";
+// labelElement.setAttribute("for", "myCheckbox");
+// labelElement.textContent = "Is Graduated?";
+// tasksContainer.appendChild(labelElement);
